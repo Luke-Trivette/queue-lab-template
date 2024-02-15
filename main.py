591e1c636015d4e4b4956de265ebafb7726f6c64
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-15 15:11:12 -0600

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        pass
+        self.cards.append(card)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        pass
+        self.cards.remove(-1)
+        
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
