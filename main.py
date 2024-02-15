9b537fd2a95f99a6ad3f271316eaa6f3bbbdb00d
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-15 15:18:33 -0600

    main.py

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        self.cards.remove(-1)
+        if self.is_empty():
+            print("Queue is empty. Cannot pop.")
+            return None
+        else:
+            return self.cards.pop(0)
         
 
 if __name__ == '__main__':
-    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+     my_queue = Queue()
+     my_queue.push("Card1")
+     my_queue.push("Card2")
+     my_queue.push("Card3")
+     popped_card = my_queue.pop()
+     if popped_card is not None:
+        print(f"Popped card: {popped_card}")
+     popped_card = my_queue.pop()
+     if popped_card is not None:
+        print(f"Popped card: {popped_card}")
+     my_queue.push("Card4")
+     my_queue.push("Card5")
+     popped_card = my_queue.pop()
+     if popped_card is not None:
+        print(f"Popped card: {popped_card}")
+
+     popped_card = my_queue.pop()
+     if popped_card is not None:
+        print(f"Popped card: {popped_card}")
+     popped_card = my_queue.pop()
+     if popped_card is not None:
+        print(f"Popped card: {popped_card}")
